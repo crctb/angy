9ef1158879c3167fd757038775253a387a088af6
crctb <35693510+crctb@users.noreply.github.com>
2019-08-07 08:34:45 +0200

    ~

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -11,21 +11,37 @@ import { HeaderComponent } from './main/header/header.component';
 import { FooterComponent } from './main/footer/footer.component';
 import { BlankyComponent } from './blanky/blanky.component';
 
+import { AppInitService } from './services/app-init.service';
+
+export function initializeApp(appInitService: AppInitService) {
+    return (): Promise<any> => {
+        return appInitService.Init();
+    }
+}
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    MainComponent,
-    HeaderComponent,
-    FooterComponent,
-    BlankyComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    NgbModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        MainComponent,
+        HeaderComponent,
+        FooterComponent,
+        BlankyComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        NgbModule
+    ],
+    providers: [
+        AppInitService,
+        {
+            provide: APP_INITIALIZER,
+            useFactory: initializeApp,
+            deps: [AppInitService],
+            multi: true
+        }
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/app-init.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AppInitService {
+
+    constructor(
+        private http: HttpClient
+    ) { }
+
+    Init() {
+
+        return new Promise<void>((resolve, reject) => {
+            console.log('AppInitService called');
+
+            this.http.get<any>('/dummy/store/user/getCurrentUser').subscribe(u => {
+                console.log(u);
+                // console.log(window.location.pathname.split("/").pop());
+                
+                let node = document.createElement('link');
+                node.href = 'assets/' + u.id + '/styles.css';
+                node.rel = 'stylesheet';
+                node.type = 'text/css';
+
+                document.getElementsByTagName('head')[0].appendChild(node);
+
+                this.http.get('assets/' + u.id + '/styles.css', {
+                    responseType: 'text',
+                    observe: 'response'
+                })
+                    .subscribe((data => {
+                        if (data === null || data.headers.get('content-type').indexOf('text/css') === -1) {
+
+                        } else {
+                            resolve();
+                        }
+                    }), error => {
+                        console.log('fuck');
+                    })
+
+            });
+
+        });
+
+    }
+
+}
